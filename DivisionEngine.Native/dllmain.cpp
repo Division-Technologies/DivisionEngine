e813b3194548069357220f5715725fa8ec78a0dc
ruccho <noboroo2000@yahoo.co.jp>
2024-05-29 23:46:26 +0900

    revise

--- DivisionEngine.Native/dllmain.cpp
+++ DivisionEngine.Native/dllmain.cpp
@@ -17,33 +17,3 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     }
     return TRUE;
 }
-
-DIVISION_EXPORT int32_t Add(int32_t a, int32_t b)
-{
-    return a + b;
-}
-
-
-DIVISION_EXPORT void TestMain()
-{
-	auto hwnd = InitWindow(TEXT("aaa"), 1280, 720);
-
-    InitD3D12(hwnd, 1280, 720);
-
-	MSG msg = {};
-	while (WM_QUIT != msg.message)
-	{
-		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE == TRUE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		else
-		{
-            if(Render() != S_OK)
-            {
-                return;
-            }
-		}
-	}
-}
